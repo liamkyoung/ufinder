d209706f164f9ffd4fbf629b22910778b86426f7
Kingston <liamkingstonyoung@gmail.com>
2022-10-09 19:14:01 -0400

    Added Header with navigation

--- pages/alternate/profile.tsx
+++ pages/alternate/profile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type { NextPage } from 'next'
 
 const Profile: NextPage = () => {
-  return <div className="">profile</div>
+  return <div className="page">profile</div>
 }
 
 export default Profile
