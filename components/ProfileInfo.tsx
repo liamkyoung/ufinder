698f4e642b31a9deda76756b7f0fae4b0bbc61c5
Kingston <liamkingstonyoung@gmail.com>
2022-10-19 21:20:18 -0400

    Added Profile Page

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -6,6 +6,7 @@ import {
   UserPlusIcon,
 } from '@heroicons/react/24/solid'
 import React from 'react'
+import Link from 'next/link'
 
 type Props = {
   name: string
@@ -14,22 +15,53 @@ type Props = {
   otherInterests: string[]
 }
 
-function ProfileInfo({}: Props) {
+function ProfileInfo({ name, age, similarInterests, otherInterests }: Props) {
   return (
-    <div className="bg-pageGray h-96 w-96 shadow-lg rounded-md p-3">
-      <div className="flex justify-between">
+    <div className="bg-gray-800 shadow-lg rounded-md p-10">
+      <div className="flex justify-between text-white">
         <div className="flex-1">
-          <ChevronLeftIcon className="h-8" />
+          <Link href="/">
+            <ChevronLeftIcon className="profileButton" />
+          </Link>
         </div>
 
         <div className="flex">
-          <UserPlusIcon className="h-8" />
-          <NoSymbolIcon className="h-8" />
-          <ChatBubbleLeftRightIcon className="h-8" />
+          <UserPlusIcon className="profileButton" />
+          <NoSymbolIcon className="profileButton" />
+          <ChatBubbleLeftRightIcon className="profileButton" />
         </div>
       </div>
       <div className="flex justify-center items-start w-full pt-5">
-        <FaceSmileIcon className="h-32" />
+        <FaceSmileIcon className="h-32 cursor-pointer text-zinc-200" />
+      </div>
+      <div className="shadow-md border-2 bg-blue-400 h-16 rounded-md text-black text-xl font-cartoon flex items-center pl-5 mt-3 mb-5 justify-between">
+        <div className="flex-1 text-2xl">{name}</div>
+
+        <div className="w-max bg-yellow-200 border-2 border-yellow-400 mr-5 p-2 rounded-md">
+          {age} years old
+        </div>
+      </div>
+      <div className="border-2 border-white rounded-md flex justify-between">
+        <div className="border-2 border-zinc-200 border-opacity-50 m-2 p-2 rounded-md">
+          <div className="text-white font-cartoon text-2xl mb-1">
+            Similar Interests:
+          </div>{' '}
+          {similarInterests?.map((interest, i) => (
+            <h1 key={i} className="font-cartoon text-white text-center">
+              {interest}
+            </h1>
+          ))}
+        </div>
+        <div className="border-2 border-zinc-200 border-opacity-50 m-2 p-2 rounded-md">
+          <div className="text-white font-cartoon text-2xl mb-1">
+            Other Interests:
+          </div>{' '}
+          {otherInterests?.map((interest, i) => (
+            <h1 key={i} className="font-cartoon text-white text-center">
+              {interest}
+            </h1>
+          ))}
+        </div>
       </div>
     </div>
   )
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,13 +3,21 @@ import type { NextPage } from 'next'
 import Header from '../components/Header'
 import HeadMetaData from '../components/HeadMetaData'
 import ProfileInfo from '../components/ProfileInfo'
+import Data from '../data/user.json'
 
 const Profile: NextPage = () => {
   return (
     <div className="page">
       <HeadMetaData title="UFinder | Profile" />
       <Header />
-      <ProfileInfo />
+      <div className="flex justify-center items-center mt-8">
+        <ProfileInfo
+          name={Data.user.name}
+          age={Data.user.age}
+          similarInterests={Data.user.similarInterests}
+          otherInterests={Data.user.otherInterests}
+        />
+      </div>
     </div>
   )
 }
